e09acb29bdee8d5b1961179d63b40a71bc78abfa
Z <z@apiary.io>
2016-02-16 12:23:23 +0100

    Pep8 nice
    https://github.com/hhatto/autopep8

--- APIBlueprint.py
+++ APIBlueprint.py
@@ -7,53 +7,59 @@ from os import path
 from subprocess import Popen, PIPE
 from sublime_plugin import TextCommand, WindowCommand
 
+
 class Text():
-        @staticmethod
-        def all(view):
-                return view.substr(sublime.Region(0, view.size()))
-        @staticmethod
-        def sel(view):
-                text = []
-                for region in view.sel():
-                        if region.empty():
-                                continue
-                        text.append(view.substr(region))
-                return "".join(text)
 
-        @staticmethod
-        def get(view):
-                text = Text.sel(view)
-                if len(text) > 0:
-                        return text
-                return Text.all(view)
+    @staticmethod
+    def all(view):
+        return view.substr(sublime.Region(0, view.size()))
 
-def run_command(cmd, args = [], source = "", cwd = None, env = None):
-  settings = sublime.load_settings('APIBlueprint.sublime-settings')
+    @staticmethod
+    def sel(view):
+        text = []
+        for region in view.sel():
+            if region.empty():
+                continue
+            text.append(view.substr(region))
+        return "".join(text)
 
-  if not type(args) is list:
-    args = [args]
+    @staticmethod
+    def get(view):
+        text = Text.sel(view)
+        if len(text) > 0:
+            return text
+        return Text.all(view)
 
-  if env is None:
-    env = {"PATH": settings.get('binDir', '/usr/local/bin')}
 
-  command = [cmd] + args
+def run_command(cmd, args=[], source="", cwd=None, env=None):
+    settings = sublime.load_settings('APIBlueprint.sublime-settings')
 
-  proc = Popen(command, env = env, cwd = cwd, stdin = PIPE, stdout = PIPE, stderr = PIPE)
-  stat = proc.communicate(input=source.encode('utf-8'))
+    if not type(args) is list:
+        args = [args]
 
-  okay = proc.returncode == 0
+    if env is None:
+        env = {"PATH": settings.get('binDir', '/usr/local/bin')}
 
-  #remove leading empty line
-  lines = stat[1].decode('UTF-8').split('\n')
-  if not lines[0]:
-    lines.pop(0)
-  sanitized_errout = '\n'.join(lines)
+    command = [cmd] + args
 
-  return {"okay": okay, "out": stat[0].decode('UTF-8'), "err": sanitized_errout}
+    proc = Popen(command, env=env, cwd=cwd,
+                 stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    stat = proc.communicate(input=source.encode('utf-8'))
+
+    okay = proc.returncode == 0
+
+    # remove leading empty line
+    lines = stat[1].decode('UTF-8').split('\n')
+    if not lines[0]:
+        lines.pop(0)
+    sanitized_errout = '\n'.join(lines)
+
+    return {"okay": okay, "out": stat[0].decode('UTF-8'), "err": sanitized_errout}
+
 
 def parse_buffer(view, args, edit):
     source = Text.all(view)
-    result = run_command("drafter", args = args, source = source)
+    result = run_command("drafter", args=args, source=source)
     body = result["err"] + '\n' + result["out"]
 
     output = view.window().new_file()
@@ -66,10 +72,14 @@ def parse_buffer(view, args, edit):
 
     output.insert(edit, 0, body)
 
+
 class ParseYamlAstCommand(TextCommand):
+
     def run(self, edit, **kwargs):
         parse_buffer(self.view, kwargs["opt"], edit)
 
+
 class ParseJsonAstCommand(TextCommand):
+
     def run(self, edit, **kwargs):
         parse_buffer(self.view, kwargs["opt"], edit)
